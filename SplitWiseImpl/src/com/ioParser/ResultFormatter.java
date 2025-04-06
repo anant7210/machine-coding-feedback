c9ceffaebf8afd87fcb74cd878ee780ffccaaf8e
Anant Akash <anant7210@gmail.com>
2025-04-06 23:01:57 +0530

    Revert "Add Javadocs"
    This reverts commit 43cae821f35f1d2be8ebe1b870b1d99f78f3dc80.

--- SplitWiseImpl/src/com/ioParser/ResultFormatter.java
+++ SplitWiseImpl/src/com/ioParser/ResultFormatter.java
@@ -1,16 +1,5 @@
 package com.ioParser;
 
-import java.util.List;
-
-import com.ioParser.interfaces.IResultFormatter;
-import com.strategies.splitObjects.SplitResult;
-
-public class ResultFormatter implements IResultFormatter{
-
-	@Override
-	public List<String> getFormattedResult(SplitResult splitResult) {
-		return null;
-		
-	}
+public class ResultFormatter {
 
 }
